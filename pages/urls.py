4ead4dae33c56a88b6cc59d45b378727f58b4bcb
robertmezack <robertmezack@gmail.com>
2021-11-09 21:07:05 -0600

    configuring for heroku

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,8 @@
 # pages/urls.py
 from django.urls import path
-from . import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('about/', views.about_page, name='about'),
-    path('', views.home_page, name='home'),
-
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 # pages/views.py
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-def home_page(request):
-    return render(request, 'home.html')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-def about_page(request):
-    return render(request, 'about.html')
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
